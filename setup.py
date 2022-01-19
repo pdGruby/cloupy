4754b16851b9919991a73363f4ecd5d864f0c0be
Kamil Grala <misiekxabi@gmail.com>
2022-01-19 19:01:22 +0100

    refactor: Update setup.py file

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.0.0'
+VERSION = '1.1.0'
 DESCRIPTION = 'The package allows to download, process and visualize climatological data from reliable sources'
 README = open('README.md', 'r', encoding='utf8').read()
 
@@ -16,14 +16,15 @@ setup(
     license='MIT',
     packages=find_packages(),
     install_requires=[
-        'pandas>=1.3.3,<=1.3.4',
-        'matplotlib>=3.4.3,<=3.4.3',
-        'requests>=2.26.0,<=2.26.0',
-        'beautifulsoup4>=4.9.3,<=4.9.3',
-        'numpy>=1.21.4,<=1.21.4',
-        'pyproj',
-        'scipy',
-        'Pillow', 'cycler'
+        'pandas>=1.3.3,<=1.3.5',
+        'matplotlib>=3.4.3,<=3.5.1',
+        'requests>=2.26.0,<=2.27.1',
+        'beautifulsoup4>=4.9.3,<=4.10.0',
+        'numpy>=1.21.4,<=1.22.1',
+        'pyproj>=3.2.1,<=3.3.0',
+        'scipy>=1.7.2,<=1.7.3',
+        'Pillow>=8.4.0,<=9.0.0',
+        'cycler==0.11.0'
     ],
     tests_require=[
         'pytest>=6.2.5',
@@ -31,6 +32,8 @@ setup(
     ],
     package_data={
         'cloupy': [
+            'data_processing/*',
+            'maps/*'
             'scraping/*',
             'diagrams/*',
             'test/test_integration/*',
@@ -39,7 +42,7 @@ setup(
                   },
     include_package_data=True,
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
@@ -48,6 +51,8 @@ setup(
         'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
         'Framework :: Matplotlib',
-        'Topic :: Scientific/Engineering :: Atmospheric Science'
+        'Topic :: Scientific/Engineering :: GIS',
+        'Topic :: Scientific/Engineering :: Atmospheric Science',
+        'Topic :: Scientific/Engineering :: Visualization'
     ]
 )
